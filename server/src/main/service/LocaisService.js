5fcaf5f869e730a0e2528175c3aef0d03415c7e5
Estácio <estacio.neto@ccc.ufcg.edu.br>
2017-07-20 22:34:04 -0300

    Só colocando umas variáveis

--- server/src/main/service/LocaisService.js
+++ server/src/main/service/LocaisService.js
@@ -1,12 +1,12 @@
 'use strict';
 
+const Local = require('../model/Local'),
+    _ = require('../util/util');
+
 export class LocaisService {
 
     constructor(db_profile = 'SCORE') {
         require('../config/db_config')(db_profile);
-
-        // const Local = require('../model/Local'),
-        //     _ = require('../util/util');
     }
 
 }
