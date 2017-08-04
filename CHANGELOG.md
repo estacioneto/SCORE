848a55320b30fad44f6c3f1771c6fd0b534ff260
Eric Breno <eric.santos@ccc.ufcg.edu.br>
2017-08-03 21:22:12 -0300

    Correção e ajuste em tipo de reserva

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# CHANGELOG
+
+# Unstable
+* Funcionalidade: Adição de tipo "Outro" e "Palestra" aos tipos de Reserva.
+* Correção: Corrigido tipo de reserva "Assembleia"
+
+# 0.0.3 Beta
+* Início versionamento.
--- client/src/main/js/constants/constantesReserva.js
+++ client/src/main/js/constants/constantesReserva.js
@@ -4,30 +4,44 @@
     angular.module('reservaModulo', []).constant('TIPOS_RESERVA', {
         "Reunião": {
             corMd: {
-                "background-color": "green-800"
+                "background-color": "indigo-A700"
             },
-            corRgb: 'rgb(46, 125, 50)',
+            corRgb: 'rgb(48, 79, 254)',
             corTexto: ''
         },
         "Videoconferência": {
             corMd: {
-                "background-color": "red"
+                "background-color": "green-600"
             },
-            corRgb: 'rgb(255, 82, 82)',
+            corRgb: 'rgb(67, 160, 71)',
             corTexto: ''
         },
-        "Assembléia": {
+        "Assembleia": {
             corMd: {
-                "background-color": "blue"
+                "background-color": "indigo-800"
             },
-            corRgb: "rgb(68, 138, 255)",
+            corRgb: "rgb(40, 53, 147)",
             corTexto: ''
         },
         "Defesa": {
             corMd: {
-                "background-color": "purple"
+                "background-color": "purple-400"
             },
-            corRgb: "rgb(224, 64, 251)",
+            corRgb: "rgb(171, 71, 188)",
+            corTexto: ''
+        },
+        "Palestra": {
+            corMd: {
+                "background-color": "red-600"
+            },
+            corRgb: "rgb(229, 57, 53)",
+            corTexto: ''
+        },
+        "Outro": {
+            corMd: {
+                "background-color": "grey-600"
+            },
+            corRgb: "rgb(117, 117, 117)",
             corTexto: ''
         }
     });
